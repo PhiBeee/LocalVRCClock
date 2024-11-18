fd86daaaa262ba52c42fe2557e4c64a39ba356c8
PhiBeee <118394737+PhiBeee@users.noreply.github.com>
2024-11-18 10:24:33 +0100

    Added all the files
    Unity Package for easy import into a World Project
    and
    C# and Udon compile files if someone just wants the script

---
+++ LocalClock.cs
@@ -0,0 +1,38 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+using TMPro;
+using System;
+
+namespace PhiBee{
+    public class LocalClock : UdonSharpBehaviour
+    {
+        public TextMeshProUGUI clock;
+        private string localTime;
+
+        public void Start(){
+            localTime = getTime();
+            
+            clock.text = localTime;
+        }
+
+        public void Update(){
+            string tmp = getTime();
+            if(tmp.CompareTo(localTime) != 0){
+                localTime = tmp;
+                clock.text = localTime;
+            }
+        }
+
+        private string getTime(){
+            var now = DateTime.Now;
+            var utcNow = now.ToUniversalTime();
+            var localOffsetMin = (int)(now - utcNow).TotalMinutes;
+            var localTimeTmp = (DateTime.UtcNow + TimeSpan.FromMinutes(localOffsetMin)).ToString("hh:mm tt");
+
+            return localTimeTmp;
+        }
+    }
+}
